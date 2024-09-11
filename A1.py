84369ff05e0ad1de7243ae18051db616deae8f1f
Mary Zhang <jianqingzhang1888@gmail.com>
2024-09-11 13:13:43 +0200

    change

--- A1.py
+++ A1.py
@@ -1,2 +1,7 @@
 import numpy as np 
 import pandas as pd 
+import openpyxl
+
+returns=pd.read_excel('C:/Users/75590/Desktop/BSC2/Master/Asset pricing/AP_Project/25_Portfolios_5x5_Wout_Div.xlsx', sheet_name='Avg Mon Value Weighted')
+
+print(returns['Date'])
